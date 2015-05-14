0f7d192532f6ece702a240372c23c79a6d7d46d9
MosabWadea <mwaamh@gmail.com>
2015-05-14 05:02:23 +0300

    added color controles and some commented code for erosion

--- COE487/MainWindow.Designer.cs
+++ COE487/MainWindow.Designer.cs
@@ -31,13 +31,46 @@
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
             this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.redL = new System.Windows.Forms.TrackBar();
+            this.redLb = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.blueLT = new System.Windows.Forms.TextBox();
+            this.greenLT = new System.Windows.Forms.TextBox();
+            this.redLT = new System.Windows.Forms.TextBox();
+            this.blueL = new System.Windows.Forms.TrackBar();
+            this.blueLb = new System.Windows.Forms.Label();
+            this.greenL = new System.Windows.Forms.TrackBar();
+            this.greenLb = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveOutputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.blueHT = new System.Windows.Forms.TextBox();
+            this.greenHT = new System.Windows.Forms.TextBox();
+            this.redHT = new System.Windows.Forms.TextBox();
+            this.blueH = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.greenH = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.redH = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redL)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.blueL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenL)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.blueH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redH)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(13, 13);
+            this.imageBox1.Location = new System.Drawing.Point(13, 38);
             this.imageBox1.Name = "imageBox1";
             this.imageBox1.Size = new System.Drawing.Size(640, 480);
             this.imageBox1.TabIndex = 2;
@@ -45,25 +78,284 @@
             // 
             // imageBox2
             // 
-            this.imageBox2.Location = new System.Drawing.Point(670, 13);
+            this.imageBox2.Location = new System.Drawing.Point(670, 38);
             this.imageBox2.Name = "imageBox2";
             this.imageBox2.Size = new System.Drawing.Size(640, 480);
             this.imageBox2.TabIndex = 3;
             this.imageBox2.TabStop = false;
             // 
+            // redL
+            // 
+            this.redL.LargeChange = 25;
+            this.redL.Location = new System.Drawing.Point(123, 31);
+            this.redL.Maximum = 255;
+            this.redL.Name = "redL";
+            this.redL.Size = new System.Drawing.Size(306, 45);
+            this.redL.SmallChange = 10;
+            this.redL.TabIndex = 4;
+            this.redL.Scroll += new System.EventHandler(this.redL_Scroll);
+            // 
+            // redLb
+            // 
+            this.redLb.AutoSize = true;
+            this.redLb.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLb.Location = new System.Drawing.Point(33, 31);
+            this.redLb.Name = "redLb";
+            this.redLb.Size = new System.Drawing.Size(66, 16);
+            this.redLb.TabIndex = 5;
+            this.redLb.Text = "Red Value";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.blueLT);
+            this.groupBox1.Controls.Add(this.greenLT);
+            this.groupBox1.Controls.Add(this.redLT);
+            this.groupBox1.Controls.Add(this.blueL);
+            this.groupBox1.Controls.Add(this.blueLb);
+            this.groupBox1.Controls.Add(this.greenL);
+            this.groupBox1.Controls.Add(this.greenLb);
+            this.groupBox1.Controls.Add(this.redL);
+            this.groupBox1.Controls.Add(this.redLb);
+            this.groupBox1.Location = new System.Drawing.Point(110, 524);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(548, 176);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lower Color Values";
+            // 
+            // blueLT
+            // 
+            this.blueLT.Location = new System.Drawing.Point(435, 127);
+            this.blueLT.Name = "blueLT";
+            this.blueLT.Size = new System.Drawing.Size(63, 20);
+            this.blueLT.TabIndex = 12;
+            this.blueLT.TextChanged += new System.EventHandler(this.blueLT_TextChanged);
+            // 
+            // greenLT
+            // 
+            this.greenLT.Location = new System.Drawing.Point(435, 77);
+            this.greenLT.Name = "greenLT";
+            this.greenLT.Size = new System.Drawing.Size(63, 20);
+            this.greenLT.TabIndex = 11;
+            this.greenLT.TextChanged += new System.EventHandler(this.greenLT_TextChanged);
+            // 
+            // redLT
+            // 
+            this.redLT.Location = new System.Drawing.Point(435, 31);
+            this.redLT.Name = "redLT";
+            this.redLT.Size = new System.Drawing.Size(63, 20);
+            this.redLT.TabIndex = 10;
+            this.redLT.TextChanged += new System.EventHandler(this.redLT_TextChanged);
+            // 
+            // blueL
+            // 
+            this.blueL.LargeChange = 25;
+            this.blueL.Location = new System.Drawing.Point(123, 127);
+            this.blueL.Maximum = 255;
+            this.blueL.Name = "blueL";
+            this.blueL.Size = new System.Drawing.Size(306, 45);
+            this.blueL.SmallChange = 10;
+            this.blueL.TabIndex = 8;
+            this.blueL.Scroll += new System.EventHandler(this.blueL_Scroll);
+            // 
+            // blueLb
+            // 
+            this.blueLb.AutoSize = true;
+            this.blueLb.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLb.Location = new System.Drawing.Point(33, 127);
+            this.blueLb.Name = "blueLb";
+            this.blueLb.Size = new System.Drawing.Size(68, 16);
+            this.blueLb.TabIndex = 9;
+            this.blueLb.Text = "Blue Value";
+            // 
+            // greenL
+            // 
+            this.greenL.LargeChange = 25;
+            this.greenL.Location = new System.Drawing.Point(123, 77);
+            this.greenL.Maximum = 255;
+            this.greenL.Name = "greenL";
+            this.greenL.Size = new System.Drawing.Size(306, 45);
+            this.greenL.SmallChange = 10;
+            this.greenL.TabIndex = 6;
+            this.greenL.Scroll += new System.EventHandler(this.greenL_Scroll);
+            // 
+            // greenLb
+            // 
+            this.greenLb.AutoSize = true;
+            this.greenLb.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLb.Location = new System.Drawing.Point(33, 77);
+            this.greenLb.Name = "greenLb";
+            this.greenLb.Size = new System.Drawing.Size(78, 16);
+            this.greenLb.TabIndex = 7;
+            this.greenLb.Text = "Green Value";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1326, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveOutputToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.aboutToolStripMenuItem.Text = "File";
+            // 
+            // saveOutputToolStripMenuItem
+            // 
+            this.saveOutputToolStripMenuItem.Name = "saveOutputToolStripMenuItem";
+            this.saveOutputToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.saveOutputToolStripMenuItem.Text = "Save Output";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.blueHT);
+            this.groupBox2.Controls.Add(this.greenHT);
+            this.groupBox2.Controls.Add(this.redHT);
+            this.groupBox2.Controls.Add(this.blueH);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.greenH);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.redH);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(664, 524);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(548, 176);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Higher Color Values";
+            // 
+            // blueHT
+            // 
+            this.blueHT.Location = new System.Drawing.Point(435, 127);
+            this.blueHT.Name = "blueHT";
+            this.blueHT.Size = new System.Drawing.Size(63, 20);
+            this.blueHT.TabIndex = 12;
+            this.blueHT.TextChanged += new System.EventHandler(this.blueHT_TextChanged);
+            // 
+            // greenHT
+            // 
+            this.greenHT.Location = new System.Drawing.Point(435, 77);
+            this.greenHT.Name = "greenHT";
+            this.greenHT.Size = new System.Drawing.Size(63, 20);
+            this.greenHT.TabIndex = 11;
+            this.greenHT.TextChanged += new System.EventHandler(this.greenHT_TextChanged);
+            // 
+            // redHT
+            // 
+            this.redHT.Location = new System.Drawing.Point(435, 31);
+            this.redHT.Name = "redHT";
+            this.redHT.Size = new System.Drawing.Size(63, 20);
+            this.redHT.TabIndex = 10;
+            this.redHT.TextChanged += new System.EventHandler(this.redHT_TextChanged);
+            // 
+            // blueH
+            // 
+            this.blueH.LargeChange = 25;
+            this.blueH.Location = new System.Drawing.Point(123, 127);
+            this.blueH.Maximum = 255;
+            this.blueH.Name = "blueH";
+            this.blueH.Size = new System.Drawing.Size(306, 45);
+            this.blueH.SmallChange = 10;
+            this.blueH.TabIndex = 8;
+            this.blueH.Scroll += new System.EventHandler(this.blueH_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Blue Value";
+            // 
+            // greenH
+            // 
+            this.greenH.LargeChange = 25;
+            this.greenH.Location = new System.Drawing.Point(123, 77);
+            this.greenH.Maximum = 255;
+            this.greenH.Name = "greenH";
+            this.greenH.Size = new System.Drawing.Size(306, 45);
+            this.greenH.SmallChange = 10;
+            this.greenH.TabIndex = 6;
+            this.greenH.Scroll += new System.EventHandler(this.greenH_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Green Value";
+            // 
+            // redH
+            // 
+            this.redH.LargeChange = 25;
+            this.redH.Location = new System.Drawing.Point(123, 31);
+            this.redH.Maximum = 255;
+            this.redH.Name = "redH";
+            this.redH.Size = new System.Drawing.Size(306, 45);
+            this.redH.SmallChange = 10;
+            this.redH.TabIndex = 4;
+            this.redH.Scroll += new System.EventHandler(this.redH_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Red Value";
+            // 
             // HelloOpenCV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1326, 583);
+            this.ClientSize = new System.Drawing.Size(1326, 715);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.imageBox2);
             this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "HelloOpenCV";
             this.Text = "Hello Open CV";
             this.Load += new System.EventHandler(this.HelloOpenCV_Load);
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redL)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.blueL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenL)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.blueH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redH)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +363,30 @@
 
         private Emgu.CV.UI.ImageBox imageBox1;
         private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.TrackBar redL;
+        private System.Windows.Forms.Label redLb;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar blueL;
+        private System.Windows.Forms.Label blueLb;
+        private System.Windows.Forms.TrackBar greenL;
+        private System.Windows.Forms.Label greenLb;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveOutputToolStripMenuItem;
+        private System.Windows.Forms.TextBox blueLT;
+        private System.Windows.Forms.TextBox greenLT;
+        private System.Windows.Forms.TextBox redLT;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox blueHT;
+        private System.Windows.Forms.TextBox greenHT;
+        private System.Windows.Forms.TextBox redHT;
+        private System.Windows.Forms.TrackBar blueH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar greenH;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar redH;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- COE487/MainWindow.cs
+++ COE487/MainWindow.cs
@@ -20,9 +20,34 @@ namespace COE487{
         bool caping = false;
         Image<Bgr, Byte> org;
         Image<Gray, Byte> proc;
+        Bgr bgrH = new Bgr();
+        Bgr bgrL = new Bgr();
 
         public HelloOpenCV(){
             InitializeComponent();
+
+            bgrH.Blue = 255;
+            bgrH.Green = 100;
+            bgrH.Red = 100;
+
+            bgrL.Blue = 175;
+            bgrL.Green = 0;
+            bgrL.Red = 0;
+
+            blueH.Value = 255;
+            greenH.Value = 100;
+            redH.Value = 100;
+
+            blueL.Value = 175;
+
+            blueHT.Text = "255";
+            greenHT.Text = "100";
+            redHT.Text = "100";
+
+            blueLT.Text = "175";
+            greenLT.Text = "0";
+            redLT.Text = "0";
+
         }
 
         private void HelloOpenCV_Load(object sender, EventArgs e){
@@ -38,31 +63,128 @@ namespace COE487{
 
         }
 
-        public void HelloOpenCV_FormClosed(object sender, FormClosedEventArgs e) {
-            if (cam != null) {
-                cam.Dispose();
-            }
-        }
+
 
         void processFrameAndUpdateGUI(object sender, EventArgs arg) {
             org = cam.QueryFrame();
             if (org == null) return;
-            //#TODO make red detection
-            proc = org.InRange(new Bgr(175, 0, 0), new Bgr(256, 100, 100));
+
+            Image<Bgr, Byte> skeleton = new Image<Bgr,Byte>(new Size(331, 244));
+
+            //org._EqualizeHist();
+            //org._GammaCorrect(0.3);
+
+            proc = org.InRange(bgrL, bgrH);
 
             proc = proc.SmoothGaussian(9);
 
             CircleF[] circles = proc.HoughCircles(new Gray(100), new Gray(50), 2, proc.Height / 4, 10, 400)[0];
 
-            foreach (CircleF circle in circles) {
-                CvInvoke.cvCircle(org, new Point((int)circle.Center.X, (int)circle.Center.Y), 3, new MCvScalar(0, 255, 0), -1, LINE_TYPE.CV_AA, 0);
+            for (int i =0; i<circles.Length; i++) {
+                CvInvoke.cvCircle(org, new Point((int)circles[i].Center.X, (int)circles[i].Center.Y), 3, new MCvScalar(0, 255, 0), -1, LINE_TYPE.CV_AA, 0);
 
-                org.Draw(circle, new Bgr(Color.Red), 3);
+                org.Draw(circles[i], new Bgr(Color.Red), 3);
             }
+
+            //proc._Erode(6);
+            //proc._Dilate(5);
+            //proc._Erode(7);
 
             imageBox1.Image = org;
             imageBox2.Image = proc;
 
+        }
+
+        //////This is the erosion method but it is still not working
+        //void erode(object sender, EventArgs arg) {
+        //    org = cam.QueryFrame();
+        //    if (org == null) return;
+
+        //    proc = org.Convert<Gray, Byte>();
+
+        //    Image<Gray, Byte> eroded = new Image<Gray, byte>(proc.Size);
+        //    Image<Gray, Byte> temp = new Image<Gray, byte>(proc.Size);
+        //    Image<Gray, Byte> skel = new Image<Gray, byte>(proc.Size);
+        //    skel.SetValue(0);
+        //    CvInvoke.cvThreshold(proc, proc, 127, 256, 0);
+        //    StructuringElementEx element = new StructuringElementEx(3, 3, 1, 1, Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_CROSS);
+        //    bool done = false;
+
+        //    while (!done) {
+        //        CvInvoke.cvErode(proc, eroded, element, 1);
+        //        CvInvoke.cvDilate(eroded, temp, element, 1);
+        //        temp = proc.Sub(temp);
+        //        skel = skel | temp;
+        //        proc = eroded;
+        //        if (CvInvoke.cvCountNonZero(org) == 0) done = true;
+        //    }
+
+        //    imageBox1.Image = org;
+        //    imageBox2.Image = proc;
+
+        //}
+
+        //set scroll values for low
+        private void redL_Scroll(object sender, EventArgs e) {
+            bgrL.Red = redL.Value;
+            redLT.Text = redL.Value +"";
+        }
+        private void greenL_Scroll(object sender, EventArgs e) {
+            bgrL.Green = greenL.Value;
+            greenLT.Text = greenL.Value + "";
+        }
+        private void blueL_Scroll(object sender, EventArgs e) {
+            bgrL.Blue = blueL.Value;
+            blueLT.Text = blueL.Value + "";
+        }
+
+        //set text values for low
+        private void redLT_TextChanged(object sender, EventArgs e) {
+            bgrL.Red = Convert.ToInt32(redLT.Text);
+            redL.Value = Convert.ToInt32(redLT.Text);
+        }
+        private void greenLT_TextChanged(object sender, EventArgs e) {
+            bgrL.Green = Convert.ToInt32(greenLT.Text);
+            greenL.Value = Convert.ToInt32(greenLT.Text);
+        }
+        private void blueLT_TextChanged(object sender, EventArgs e) {
+            bgrL.Blue = Convert.ToInt32(blueLT.Text);
+            blueL.Value = Convert.ToInt32(blueLT.Text);
+        }
+
+        //set scroll values for high
+        private void redH_Scroll(object sender, EventArgs e) {
+            bgrH.Red = redH.Value;
+            redHT.Text = redH.Value + "";
+        }
+        private void greenH_Scroll(object sender, EventArgs e) {
+            bgrH.Green = greenH.Value;
+            greenHT.Text = greenH.Value + "";
+        }
+        private void blueH_Scroll(object sender, EventArgs e) {
+            bgrH.Blue = blueH.Value;
+            blueHT.Text = blueH.Value + "";
+        }
+
+        //set text values for high
+        private void redHT_TextChanged(object sender, EventArgs e) {
+            bgrH.Red = Convert.ToInt32(redHT.Text);
+            redH.Value = Convert.ToInt32(redHT.Text);
+        }
+        private void greenHT_TextChanged(object sender, EventArgs e) {
+            bgrH.Green = Convert.ToInt32(greenHT.Text);
+            greenH.Value = Convert.ToInt32(greenHT.Text);
+        }
+        private void blueHT_TextChanged(object sender, EventArgs e) {
+            bgrH.Blue = Convert.ToInt32(blueHT.Text);
+            blueH.Value = Convert.ToInt32(blueHT.Text);
+        }
+
+
+        public void HelloOpenCV_FormClosed(object sender, FormClosedEventArgs e) {
+            if (cam != null) {
+                cam.Dispose();
+            }
         }
     }
 }
